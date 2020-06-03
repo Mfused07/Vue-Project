ed0bc2d19d46c142cd5c27a5d29b8e16167fcbd2
Mfused07 <p180143@nu.edu.pk>
2020-06-03 22:42:29 +0500

    initComit

---
+++ src/views/Products.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="card">
+    <img src='../assets/img/products/product3.jpg' alt="Denim Jeans" style="width:40%" >
+    <h1>Tailored Jeans</h1>
+    <p class="price">$19.99</p>
+    <p>Some text about the jeans..</p>
+    <p><button>Add to Cart</button></p>
+  </div>
+
+</template>
+
+  <script>
+
+
+     export default {
+
+      
+      
+    }
+  </script>
+
+  <style scoped>
+  </style>
